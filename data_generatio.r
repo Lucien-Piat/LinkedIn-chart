c437e8a8319f81325e827592045070c65360f225
Lpiat <lucienpiat33@gmail.com>
2024-05-22 18:05:35 +0200

    Added file to generate random values

---
+++ data_generatio.r
@@ -0,0 +1,38 @@
+
+set.seed(42)
+
+generate_matrix <- function(type, center) {
+  # Generate random values
+  random_values <- rnorm(10, mean = center, sd=0.1) 
+  
+  # Create a matrix with type and random values
+  matrix_data <- cbind(rep(type, 10), random_values)
+  colnames(matrix_data) <- c("Type", "Time")
+  
+  return(matrix_data)
+}
+
+data = rbind(generate_matrix('Coffee', 7),
+    generate_matrix('Coffee', 7),
+    generate_matrix('Coffee', 7),
+    generate_matrix('Coffee', 7),
+    generate_matrix('Coffee', 11),
+    generate_matrix('Coffee', 11),
+    generate_matrix('Coffee', 11),
+    generate_matrix('Coffee', 14),
+    generate_matrix('Sleep', 0),
+    generate_matrix('Sleep', 24),
+    generate_matrix('R', 14),
+    generate_matrix('R', 15),
+    generate_matrix('R', 16),
+    generate_matrix('R', 16),
+    generate_matrix('R', 17),
+    generate_matrix('Java', 10),
+    generate_matrix('Java', 9),
+    generate_matrix('Java', 13),
+    generate_matrix('Python', 20),
+    generate_matrix('Python', 22),
+    generate_matrix('Python', 20)
+    )
+
+write.csv(data, file = "data/data.csv", row.names = FALSE)
